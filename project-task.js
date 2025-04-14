4c53915855ca9856b2880c07b76c4982bce79fb7
Kdiviya <eraiswaryabe@gmail.com>
2025-04-14 17:40:17 -0500

    Add project-task.js

--- project-task.js
+++ project-task.js
@@ -44,6 +44,18 @@ Step-by-Step:
 3. Return the filtered result.
 */
 
+// priceCondition is the callback which is a parameter to the filterProducts(), which filters the product with price >=500.
+function filterProducts(product, priceCondition){ 
+  console.log("Filtered products:");
+  const filteredProducts = product.filter(priceCondition);
+  return filteredProducts;
+}
+
+function filterPrice(productsParam){
+   return productsParam.price >=500;
+}
+
+console.log(filterProducts(products,filterPrice));
 
 /*
 🔹 Task 2: Transform Product Names
@@ -56,6 +68,9 @@ Step-by-Step:
 3. Store the result in a new variable.
 */
 
+let upperCaseNames = products.map(nameParam => nameParam.name.toUpperCase()); // used map() to transform the product name in uppercase.
+console.log("Uppercased names:");
+console.log(upperCaseNames);
 
 /*
 🔹 Task 3: Generate Discounted Prices
@@ -70,6 +85,19 @@ Step-by-Step:
 3. Use this returned function inside a `map()` call to apply discounts to all products.
 */
 
+// created  a higher order function applyDiscount() which accepts the parameter as function(product) to calculate the discounted product price.
+function applyDiscount(discountParam){
+  return (function(productParam){
+    return{ 
+      name: productParam.name,
+      price: productParam.price - (productParam.price *(discountParam/100))};
+  }
+);}
+
+const discountPercent = applyDiscount(10); // called applyDiscount() and assign the callback "function(product)" to the const variable "discountPercent".
+console.log("Discounted products:");
+console.log(products.map(discountPercent)); //This calls the callback function(product) inside the map() and display the product name and discount price.
+
 
 /*
 🔹 Task 4: Calculate Total Inventory Value
@@ -82,6 +110,14 @@ Step-by-Step:
 3. Store the total in a new variable.
 */
 
+let totalVal = products
+  .filter((product) => product.inStock) //used filter() to list the instocked products.
+
+  //used reduce() to calculate the total price of the instocked products.
+  .reduce((total, prod) => {
+  const priceConversion = Number(prod.price);
+  return total + priceConversion; }, 0);
+  console.log("Total value in stock: " +totalVal);
 
 // ============================================
 // 🧪 Console Test Your Work
@@ -91,3 +127,21 @@ Step-by-Step:
 // console.log("Uppercased names:", ...);
 // console.log("Discounted products:", ...);
 // console.log("Total value in stock:", ...);
+/*output:
+Filtered products:
+[
+  { name: 'Laptop', price: 1000, inStock: true },
+  { name: 'Phone', price: 500, inStock: false },
+  { name: 'Tablet', price: 800, inStock: true }
+]
+Uppercased names:
+[ 'LAPTOP', 'PHONE', 'TABLET', 'MONITOR', 'KEYBOARD' ]
+Discounted products:
+[
+  { name: 'Laptop', price: 900 },
+  { name: 'Phone', price: 450 },
+  { name: 'Tablet', price: 720 },
+  { name: 'Monitor', price: 270 },
+  { name: 'Keyboard', price: 90 }
+]
+Total value in stock: 2100 */
